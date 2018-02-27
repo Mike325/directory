525c9df7753ed19714755294ad78e39904c32bb7
ARMANDO VILLARREAL <14172484+eduarmando@users.noreply.github.com>
2018-02-26 20:02:54 -0600

    Update main.py

--- python_console_crud/main.py
+++ python_console_crud/main.py
@@ -1,94 +1,59 @@
-#!/usr/bin/python
 # coding:utf-8
 
-from functions import *
+from data import *
 
 
-# ==================== CREATE ====================
-def create_record():
-    name = raw_input("Enter name: ")
-    last_name = raw_input("Enter last name: ")
-    address = raw_input("Enter address: ")
-    cellphone = raw_input("Enter cellphone: ")
-    email = raw_input("Enter email address: ")
-    social_network = raw_input("Enter social network: ")
+def get_spaces():
     for record in elements:
-        if set([name, last_name, address, cellphone, email, social_network]).issubset(record.values()):
-            print ("Record: {name} {last_name} {cellphone} is already in the database")
-            return
-    if name and last_name and cellphone:
-        elements.append({
-            "id": get_next_id(),
-            "name": name,
-            "last_name": last_name,
-            "address": address,
-            "cellphone": cellphone,
-            "email": email,
-            "social_network": social_network
-        })
-        print("User added successfully")
-    else:
-        print ("You have added empty values") 
+        for key, value in record.items():
+            value_length = len(value)
+            translit_length = len(translits[key])
+
+            if spaces[key] < value_length or spaces[key] < translit_length:
+                if value_length > translit_length:
+                    spaces[key] = value_length
+                else:
+                    spaces[key] = translit_length
+    return spaces
+
+
         
-        
-# ==================== UPDATE ====================
-def update_record():
+def print_to_console():
     if elements:
-        print_to_console()
-        record_id = raw_input("Input the user key: ")
-        for record in elements:
-            if record["id"] == record_id:
-                for key, value in record.items():
-                    if key == "id":
-                        continue
-                    new_record = raw_input(
-                        "Actual Value -> {current_name}: {current_record}, Enter new value or leave it empty: ".format(
-                            current_name = translits.get(key), current_record = value
-                        )
-                    )
-                    if new_record:
-                        record[key] = new_record
-                        print ("User updated successfully")
-                break
-        else:
-            print ("User not found")
+        print ("========================================================================================================")
+        spaces = get_spaces()
+        print ("| id | Name{name_space}| Last Name{last_name_space}| Address{address_space}| Cellphone{cellphone_space}| Email{email_space}| Social Network{social_network_space}|").format(
+            name_space = " " * (spaces["name"]-len(translits["name"])+1),
+            last_name_space = " " * (spaces["last_name"]-len(translits["last_name"])+1),
+            address_space = " " * (spaces["address"]-len(translits["address"])+1),
+            cellphone_space = " " * (spaces["cellphone"]-len(translits["cellphone"])+1),
+            email_space = " " * (spaces["email"]-len(translits["email"])+1),
+            social_network_space = " " * (spaces["social_network"]-len(translits["social_network"])+1),
+        )
+        for element in elements:
+            id_space = " " * (spaces["id"]-len(element["id"]))
+            name_space = " " * (spaces["name"]-len(element["name"]))
+            last_name_space = " " * (spaces["last_name"]-len(element["last_name"]))
+            address_space = " " * (spaces["address"]-len(element["address"]))
+            cellphone_space = " " * (spaces["cellphone"]-len(element["cellphone"]))
+            email_space = " " * (spaces["email"]-len(element["email"]))
+            social_network_space = " " * (spaces["social_network"]-len(element["social_network"]))
+            print ("| {id} | {name} | {last_name} | {address} | {cellphone} | {email} | {social_network} |").format(
+                id = element["id"]+id_space,
+                name = element["name"] + name_space,
+                last_name = element["last_name"] + last_name_space,
+                address = element["address"] + address_space,
+                cellphone = element["cellphone"] + cellphone_space,
+                email = element["email"] + email_space,
+                social_network = element["social_network"] + social_network_space
+            )
+        print ("========================================================================================================")
     else:
-        print ("No elements to show")
+        print ("Empty directory")
 
 
-# ==================== DELETE ====================
-def delete_record():
-    if elements:
-        print_to_console()
-        record_id = raw_input("Enter user ID: ")
-        for record in elements:
-            if record["id"] == record_id:
-                elements.remove(record)
-                print ("User deleted successfully")
-                break
-        else:
-            print ("User not found")
-    else:
-        print ("No elements to show")
-
-
-# ==================== MAIN ====================
-def main():
-    while True:
-        action = raw_input(MAIN_MENU)
-        if action == "1":
-            create_record()
-        elif action == "2":
-            print_to_console()
-        elif action == "3":
-            update_record()
-        elif action == "4":
-            delete_record()
-        elif action == "E" or "e":
-            break
-        else:
-            print ("Sin acción en {action}").format(action=action)
-
-
-if __name__ == "__main__":
-    main()
+def get_next_id():
+    if len(elements):
+        max_id = int(max(i["id"] for i in elements))
+        return str(max_id + 1)
+    return 0
