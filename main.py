a3e258972e31332ff59c632f9915186f175b71d4
Mike <mike@prodeveloper.me>
2018-02-26 21:34:04 -0800

    WIP porting to python 3 and fixing Errors/Warning

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
+# coding:utf-8
 
 import argparse
 from logger.messages import status
-from logger.messages import error
-from logger.messages import verbose
 from logger import messages
+from database.data import MAIN_MENU
+from database.data import elements
+from database.data import translits
+from cli.functions import get_next_id
+from cli.functions import print_to_console
+# from database.data import spaces
+# from cli.functions import get_spaces
+# from logger.messages import error
+# from logger.messages import verbose
 
 __header__ = """
                               -`
@@ -12,7 +20,7 @@ __header__ = """
            .+++s+   .h`.    `ooo/
           `+++%++  .h+++   `+oooo:
           +++o+++ .hhs++. `+oooooo:
-          +s%%so%.hohhoo'  'oooooo+:
+          +s%%so%.hohhoo"  "oooooo+:
           `+ooohs+h+sh++`/:  ++oooo+:
            hh+o+hoso+h+`/++++.+++++++:
             `+h+++h.+ `/++++++++++++++:
@@ -22,7 +30,7 @@ __header__ = """
                   -osssssso.      :ssss``to.
                  :osssssss/  Mike  osssl   +
                 /ossssssss/   8a   +sssslb
-              `/ossssso+/:-        -:/+ossss'.-
+              `/ossssso+/:-        -:/+ossss".-
              `+sso+:-`                 `.-/+oso:
             `++:.                           `-/+/
             .`                                 `/
@@ -42,7 +50,7 @@ def __parse_arguments():
                         help="The T size of the tree",
                         required=False,
                         default=2,
-                        metavar='TSIZE',
+                        metavar="TSIZE",
                         dest="tsize",
                         type=int)
     parser.add_argument("-v",
@@ -74,6 +82,68 @@ def __parse_arguments():
     return cli_args
 
 
+# ==================== CREATE ====================
+def create_record():
+    name = raw_input("Enter name: ")
+    last_name = raw_input("Enter last name: ")
+    address = raw_input("Enter address: ")
+    cellphone = raw_input("Enter cellphone: ")
+    email = raw_input("Enter email address: ")
+    social_network = raw_input("Enter social network: ")
+    for record in elements:
+        if set([name, last_name, address, cellphone, email, social_network]).issubset(record.values()):
+            print("Record: {name} {last_name} {cellphone} is already in the database")
+            return
+    if name and last_name and cellphone:
+        elements.append({"id": get_next_id(),
+                         "name": name,
+                         "last_name": last_name,
+                         "address": address,
+                         "cellphone": cellphone,
+                         "email": email,
+                         "social_network": social_network})
+        print("User added successfully")
+    else:
+        print("You have added empty values")
+
+
+# ==================== UPDATE ====================
+def update_record():
+    if elements:
+        print_to_console()
+        record_id = raw_input("Input the user key: ")
+        for record in elements:
+            if record["id"] == record_id:
+                for key, value in record.items():
+                    if key == "id":
+                        continue
+                    new_record = raw_input("Actual Value -> {current_name}: {current_record}, Enter new value or leave it empty: ".format(current_name=translits.get(key), current_record=value))
+                    if new_record:
+                        record[key] = new_record
+                        print("User updated successfully")
+                break
+        else:
+            print("User not found")
+    else:
+        print("No elements to show")
+
+
+# ==================== DELETE ====================
+def delete_record():
+    if elements:
+        print_to_console()
+        record_id = raw_input("Enter user ID: ")
+        for record in elements:
+            if record["id"] == record_id:
+                elements.remove(record)
+                print("User deleted successfully")
+                break
+        else:
+            print("User not found")
+    else:
+        print("No elements to show")
+
+
 def main():
     """Main CLI function
     :returns: TODO
@@ -91,6 +161,22 @@ def main():
     messages.debug_mode = cli_args.verbose
     messages.quiet = cli_args.quiet
     messages.logger = cli_args.logger
+
+    action = ""
+
+    # While True is always a bad idea
+    while action.upper() != "E":
+        action = raw_input(MAIN_MENU)
+        if action == "1":
+            create_record()
+        elif action == "2":
+            print_to_console()
+        elif action == "3":
+            update_record()
+        elif action == "4":
+            delete_record()
+        elif action.upper() != "E":
+            print("Sin acción en {action}".format(action=action))
 
 
 if __name__ == "__main__":
